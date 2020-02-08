c4a4f5c7246cb69434fae87782bd9964d2ff34d4
D-Pow <djp460@nyu.edu>
2020-02-08 12:38:43 -0500

    Update mocks/ config files with recent improvements in MockRequests demo

--- mocks/DynamicResponses.js
+++ mocks/DynamicResponses.js
@@ -1,30 +1,39 @@
-import { kitsuTitleSearchUrl } from 'services/Urls';
+import MockRequests from 'mock-requests';
 import {
-    kimiNoNaWaSearchQuery,
-    narutoSearchQuery,
-    bleachSearchQuery,
-    fullmetalSearchQuery,
-    attackOnTitanSearchQuery
-} from './Urls';
-import {
-    kimiNoNaWaResponse,
-    narutoResponse,
-    bleachResponse,
-    fullmetalResponse,
-    attackOnTitanResponse
-} from './StaticResponses';
+    getKitsuTitleSearchUrl,
+    kitsuSearchQueryParamKey,
+    queryParamResponseMap
+} from './UrlResponseMappings';
 
-const queryResponseMap = {
-    [kimiNoNaWaSearchQuery]: kimiNoNaWaResponse,
-    [narutoSearchQuery]: narutoResponse,
-    [bleachSearchQuery]: bleachResponse,
-    [fullmetalSearchQuery]: fullmetalResponse,
-    [attackOnTitanSearchQuery]: attackOnTitanResponse
-};
+/**
+ * Dynamic response function used to determine what response to return based on network request.
+ * Will serve as the `mockResponseConfig.dynamicResponseModFn` in the
+ * `MockRequests.configureDynamicResponses()` call.
+ *
+ * Here, we use the `queryParamMap` from the requested URL to determine if the title the user
+ * searched for is mocked or not.
+ *
+ * If it is mocked, return the mock response. Otherwise, make an actual `fetch` call to get the
+ * response from the back-end.
+ *
+ * @param {*} request - Request payload in POST.
+ *     Not used because searches are done via query parameters.
+ * @param {*} response - The previous response returned by `dynamicResponseModFn`.
+ *     Not used because we generate our own response from the `queryParamMap`.
+ * @param {Object} queryParamMap - Key-Value mapping from requested URL.
+ *     Used to determine what the user searched for because search queries for Kitsu are done in the
+ *     `?filter[text]=` query parameter.
+ * @returns {Promise<{}>} - The response for the network request.
+ *     Either the mock response (if search value is mocked) or actual fetch request (if not mocked).
+ */
+export async function chooseMockBasedOnQuery(request, response, queryParamMap) {
+    // queryParamMap = { [kitsuSearchQueryParamKey]: 'what the user searched' }
+    const searchQuery = decodeURIComponent(queryParamMap[kitsuSearchQueryParamKey]);
+    const queryIsMocked = searchQuery in queryParamResponseMap;
 
-const searchQueryParam = kitsuTitleSearchUrl.substring(kitsuTitleSearchUrl.indexOf('?') + 1, kitsuTitleSearchUrl.indexOf('='));
+    if (!queryIsMocked) {
+        return await MockRequests.originalFetch(getKitsuTitleSearchUrl(searchQuery)).then(res => res.json());
+    }
 
-export function chooseMockBasedOnQuery(request, response, queryParamMap) {
-    const searchQuery = decodeURIComponent(queryParamMap[searchQueryParam]);
-    return queryResponseMap[searchQuery];
+    return queryParamResponseMap[searchQuery];
 }

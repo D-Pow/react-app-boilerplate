5d774e572cfb6d3c05f052b1f855886271c6f6cc
D-Pow <D-Pow@users.noreply.github.com>
2020-02-09 16:52:19 -0500

    Add 'fetch' polyfill
    isomorphic-fetch is based off GitHub's 'whatwg-fetch' except it adds support for NodeJS applications as well, so use that in case an app based off this boilerplate wishes to use fetch in their back-end code

--- webpack.config.js
+++ webpack.config.js
@@ -117,7 +117,7 @@ module.exports = {
         ]
     },
     entry: {
-        client: [ 'core-js', './src/index.js', ...resolvedMocks.entry ],
+        client: [ 'core-js', 'isomorphic-fetch', './src/index.js', ...resolvedMocks.entry ],
         vendor: ['react', 'react-dom', 'react-router-dom', 'prop-types']
     },
     output: {

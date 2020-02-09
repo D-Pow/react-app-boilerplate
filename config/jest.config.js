5033bd1896b929f13a5cc3ba6d65ae9358fb9ab7
D-Pow <D-Pow@users.noreply.github.com>
2020-02-09 17:10:58 -0500

    Add (local|session)Storage mocks for jest

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,12 +3,44 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+class StorageMock {
+    storage = {};
+
+    setItem(key, val) {
+        this.storage[key] = val;
+    }
+
+    getItem(key) {
+        return (key in this.storage) ? this.storage[key] : null;
+    }
+
+    removeItem(key) {
+        delete this.storage[key];
+    }
+
+    clear() {
+        this.storage = {};
+    }
+
+    get length() {
+        return Object.keys(this.storage).length;
+    }
+
+    key(index) {
+        return Object.keys(this.storage)[index] || null;
+    }
+}
+
+global.localStorage = new StorageMock();
+global.sessionStorage = new StorageMock();
+
 global.XMLHttpRequest = jest.fn(() => {
     return {
         open: () => {},
         send: () => {}
     };
 });
+
 global.fetch = jest.fn(() => Promise.resolve({
     json: () => ({ realFetchResponse: 'realFetchResponse' }),
     text: () => 'realFetchResponse'

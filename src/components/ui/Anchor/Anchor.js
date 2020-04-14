582eaf3a8969d3b11cafed3b200a67cc97d3b6f9
D-Pow <djp460@nyu.edu>
2020-04-14 16:37:26 -0400

    Extract 'target' and 'rel' attributes to props

--- src/components/ui/Anchor/Anchor.js
+++ src/components/ui/Anchor/Anchor.js
@@ -3,23 +3,42 @@ import PropTypes from 'prop-types';
 
 function Anchor(props) {
     const cls = [ props.className ];
+    const rel = [];
 
     if (props.underlineText) {
         cls.push('underline');
     }
 
+    if (typeof props.rel === typeof '') {
+        rel.push(props.rel);
+    } else if (typeof props.rel === typeof []) {
+        rel.push(...props.rel);
+    }
+
     return (
-        <a className={cls.join(' ')} href={props.href} target="_blank" rel="noopener noreferrer" {...props.aria}>
+        <a className={cls.join(' ')} href={props.href} target={props.target} rel={rel.join(' ')} {...props.aria}>
             {props.children}
         </a>
     );
 }
+
+Anchor.Targets = {
+    NEW_TAB: '_blank',
+    SAME_TAB: '_self',
+    PARENT: '_parent',
+    TOP: '_top'
+};
 
 Anchor.propTypes = {
     className: PropTypes.string,
     href: PropTypes.string,
     children: PropTypes.node,
     underlineText: PropTypes.bool,
+    rel: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string)
+    ]),
+    target: PropTypes.string,
     aria: PropTypes.object
 };
 
@@ -28,6 +47,8 @@ Anchor.defaultProps = {
     href: '',
     children: '',
     underlineText: true,
+    rel: [ 'noopener', 'noreferrer' ],
+    target: Anchor.Targets.NEW_TAB,
     aria: {}
 };
 

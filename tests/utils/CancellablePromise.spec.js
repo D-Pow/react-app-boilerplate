4e6adfa24531c1033f0354ea6b480643c2dafdee
D-Pow <djp460@nyu.edu>
2021-03-05 21:03:27 -0700

    Add new CancellablePromise logic and base layout for unit tests

---
+++ tests/utils/CancellablePromise.spec.js
@@ -0,0 +1,19 @@
+import CancellablePromise from 'utils/CancellablePromise';
+
+describe('CancellablePromise', () => {
+    it('should work as normal if no cancel() called', async () => {
+        // TODO
+    });
+
+    it('should cancel .then() statements', async () => {
+        // TODO
+    });
+
+    it('should cancel .catch() statements', async () => {
+        // TODO
+    });
+
+    it('should still call .finally() regardless of .cancel() or not', async () => {
+        // TODO
+    });
+});

31107abeb15d69a0942f8e827b0c5541dbbbb7f7
D-Pow <D-Pow@users.noreply.github.com>
2021-03-30 19:42:11 -0400

    Add new Xml.js util file for parsing XML from Base64 encoded strings

---
+++ src/utils/Xml.js
@@ -0,0 +1,44 @@
+export function decodeBase64(base64 = '') {
+    try {
+        return atob(base64);
+    } catch(e) {
+        // Could not decode, likely a malformed Base64 string
+    }
+
+    return null;
+}
+
+export function getBase64StringFromDataUrl(dataUrl = '') {
+    return dataUrl.replace(/(.*?base64,)/, '');
+}
+
+export function getMimeTypeFromDataUrl(dataUrl = '') {
+    try {
+        return dataUrl.match(/(?<=data:)[^;]+/)[0];
+    } catch(e) {
+        // Could not find match, likely a malformed data URL
+    }
+
+    return null;
+}
+
+export function getXmlTextFromDataUrl(dataUrl) {
+    return decodeBase64(getBase64StringFromDataUrl(dataUrl));
+}
+
+/**
+ * Gets a {@code Document} from the passed {@code dataUrl}.
+ *
+ * Data URLs formatting: data:MIME_TYPE;base64,BASE_64_STRING
+ * e.g. data:image/svg+xml;base64,PD94bWwgdm...
+ *
+ * @param {string} dataUrl - Data URL to parse into XML.
+ * @returns {Document} - Respective Document object generated from the retrieved XML.
+ */
+export function getXmlDocFromDataUrl(dataUrl) {
+    const mimeType = getMimeTypeFromDataUrl(dataUrl);
+    const xmlText = getXmlTextFromDataUrl(dataUrl);
+    const xmlParser = new DOMParser();
+
+    return xmlParser.parseFromString(xmlText, mimeType || 'text/xml');
+}

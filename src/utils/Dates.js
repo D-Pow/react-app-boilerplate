ec5df4efe7690ce45d2ce54f9303e8246f415a65
D-Pow <D-Pow@users.noreply.github.com>
2021-05-21 18:48:54 -0400

    Add new utils for easy handling of dates/times

---
+++ src/utils/Dates.js
@@ -0,0 +1,32 @@
+const Milliseconds = 1;
+const Seconds = 1000 * Milliseconds;
+const Minutes = 60 * Seconds;
+const Hours = 60 * Minutes;
+const Days = 24 * Hours;
+const Weeks = 7 * Days;
+const Years = 52 * Weeks;
+
+export const TimeInMillis = {
+    Milliseconds,
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Weeks,
+    Years
+};
+
+export function getDate(dateStr) {
+    if (!dateStr) {
+        return new Date();
+    }
+
+    return new Date(Date.parse(dateStr));
+}
+
+export function getNumDaysBetween(earlierDateStr, laterDateStr, asDate = true) {
+    const earlierDate = getDate(earlierDateStr);
+    const laterDate = getDate(laterDateStr);
+
+    return (laterDate - earlierDate) / TimeInMillis.Days;
+}

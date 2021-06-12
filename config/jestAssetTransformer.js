c723a1cfe5b5db4caa0e8108baba1ab4db076b46
D-Pow <D-Pow@users.noreply.github.com>
2021-06-11 18:18:01 -0600

    Minor: Rename variables and improve comments for clarity of purpose/usage

--- config/jestAssetTransformer.js
+++ config/jestAssetTransformer.js
@@ -1,56 +1,83 @@
+/* eslint-disable no-unused-vars */
+
 const path = require('path');
 const JestCssModulesTransformer = require('jest-css-modules-transform');
 const { FileTypeRegexes } = require('./utils');
 
+
 /** @type {import('@jest/core/node_modules/@jest/transform/build/types').SyncTransformer} */
 module.exports = {
     /**
-     * @param {string} fileContents
-     * @param {string} filePath
+     * Processes source file contents to be usable in jest tests.
+     * Since jest can't parse/understand all file types even after setting certain config
+     * values, this transformer file helps to transpile and/or reformat the code that will
+     * be outputted to jest test suites/files.
+     *
+     * Note: Cannot use `export default`, must use `module.exports` since this is `process()`, not `processAsync()`.
+     *
+     * @param {string} srcFileContents - Contents of the original source file, whether as a string or bytes.
+     * @param {string} srcAbsPath - Absolute path to the source file.
      * @param {Object} options
+     * @param {Object} options.config - Jest configuration options from jest.config.json, package.json->jest, etc.
+     * @param {Object} options.transformerConfig - Configuration options for this specific transformer, passed in jestConfig->transform.
      * @returns {import('@jest/types/build/Transform').TransformResult}
      */
-    process(fileContents, filePath, options) {
-        // Note: Cannot use `export default`, must use `module.exports` since this is `process()`, not `processAsync()`
+    process(srcFileContents, srcAbsPath, options) {
+        const srcText = JSON.stringify(srcFileContents);
+        const srcFileName = JSON.stringify(path.basename(srcAbsPath));
+        const {
+            config: jestConfigs,
+            transformerConfig: transformerConfigs
+        } = options;
 
-        // Text files are processed by webpack to import the file content directly as a string,
-        // so reflect that in jest tests by exporting the stringified file contents.
-        if (FileTypeRegexes.Text.test(filePath)) {
+        if (FileTypeRegexes.Text.test(srcAbsPath)) {
+            /**
+             * Text files are processed by webpack to import the file content directly as a string,
+             * so reflect that in jest tests by exporting the stringified file contents.
+             */
             return {
-                code: `module.exports = ${JSON.stringify(fileContents)};`
+                code: `module.exports = ${srcText};`
             };
         }
 
-        if (FileTypeRegexes.Svg.test(filePath)) {
+        if (FileTypeRegexes.Svg.test(srcAbsPath)) {
+            /**
+             * Mimic default (URL) and named (React.Component) exports from @svgr.
+             *
+             * Note that the String constructor must be used so we can set new fields on it in order to have both
+             * default/named exports because you can't set new fields on string primitives.
+             * i.e. let str = 'Hi'; str.myField = 'Bye'; --> str.myField == null
+             */
             return {
                 code: `
                     const React = require('react');
 
-                    // Mimic default (URL) and named (React.Component) exports from @svgr.
-                    // String constructor must be used so we can set new fields on it, which can't be done with string primitives,
-                    // i.e. let str = 'Hi'; str.myField = 'Bye'; --> str.myField == null
-                    module.exports = new String(${JSON.stringify(path.basename(filePath))});
+                    module.exports = new String(${srcFileName});
                     module.exports.ReactComponent = React.forwardRef((props, ref) => React.createElement('svg', { ref, ...props }));
                 `
             };
         }
 
-        if (FileTypeRegexes.Styles.test(filePath)) {
+        if (FileTypeRegexes.Styles.test(srcAbsPath)) {
             /**
              * `jest-css-modules-transform` has a bug where they [don't support jest@>=27]{@link https://github.com/Connormiha/jest-css-modules-transform/issues/39}.
              * So nest its call here to get the config option they are supposed to use until the bug is fixed.
+             *
+             * Another possible option: [postcss-modules-scope]{@link https://www.npmjs.com/package/postcss-modules-scope}
              */
-            const allTranspiledCssCode = JestCssModulesTransformer.process(fileContents, filePath, options.config);
+            const allTranspiledCssCode = JestCssModulesTransformer.process(srcFileContents, srcAbsPath, jestConfigs);
 
             return {
                 code: allTranspiledCssCode
             };
         }
 
-        // Binary files (images, fonts, etc.) are processed by webpack to use URLs instead of file content,
-        // so reflect that in jest tests by exporting only the name to form a "pretend" URL.
+        /**
+         * Binary files (images, fonts, etc.) are processed by webpack to use URLs instead of file content,
+         * so reflect that in jest tests by exporting only the name to form a "pretend" URL.
+         */
         return {
-            code: `module.exports = ${JSON.stringify(path.basename(filePath))};`
+            code: `module.exports = ${srcFileName};`
         };
     }
 };

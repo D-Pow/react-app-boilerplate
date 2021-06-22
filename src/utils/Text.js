8884b28e7c21214551e037cef18f66f1d0272f09
D-Pow <djp460@nyu.edu>
2021-06-21 22:33:00 -0600

    Add JSDoc for Text.js util functions
    Also, rename function to better reflect its purpose

--- src/utils/Text.js
+++ src/utils/Text.js
@@ -18,10 +18,28 @@ export function decodeBase64(base64 = '') {
     return null;
 }
 
+/**
+ * Extracts the content string from a Base64 data URL.
+ *
+ * e.g. `aGVsbG8gd29ybGQ=` would be extracted
+ * from `data:image/svg+xml;base64,aGVsbG8gd29ybGQ=`.
+ *
+ * @param {string} dataUrl - Base64 data URL, including the `data`/`base64` header content.
+ * @returns {string} - The Base64 content from the data URL.
+ */
 export function getBase64StringFromDataUrl(dataUrl = '') {
     return dataUrl.replace(/(.*?base64,)/, '');
 }
 
+/**
+ * Extracts the mime type from a Base64 data URL.
+ *
+ * e.g. `image/svg+xml` would be extracted
+ * from `data:image/svg+xml;base64,aGVsbG8gd29ybGQ=`.
+ *
+ * @param {string} dataUrl - Base64 data URL, including the `data`/`base64` header content.
+ * @returns {string} - The mime type from the data URL.
+ */
 export function getMimeTypeFromDataUrl(dataUrl = '') {
     try {
         return dataUrl.match(/(?<=data:)[^;]+/)[0];
@@ -32,22 +50,32 @@ export function getMimeTypeFromDataUrl(dataUrl = '') {
     return null;
 }
 
-export function getTextFromDataUrl(dataUrl) {
+/**
+ * Decodes the content string from a Base64 data URL for direct usage.
+ *
+ * e.g. `hello world` would be extracted
+ * from `data:text/plain;base64,aGVsbG8gd29ybGQ=`.
+ *
+ * @param {string} dataUrl - Base64 data URL, including the `data`/`base64` header content.
+ * @returns {string} - The decoded content string from the data URL.
+ */
+export function getDecodedTextFromDataUrl(dataUrl) {
     return decodeBase64(getBase64StringFromDataUrl(dataUrl));
 }
 
 /**
  * Gets a {@code Document} from the passed {@code dataUrl}.
  *
- * Data URLs formatting: data:MIME_TYPE;base64,BASE_64_STRING
- * e.g. data:image/svg+xml;base64,PD94bWwgdm...
+ * Data URLs formatting: `data:MIME_TYPE;base64,BASE_64_STRING`.
+ *
+ * e.g. `data:image/svg+xml;base64,aGVsbG8gd29ybGQ=`
  *
  * @param {string} dataUrl - Data URL to parse into XML.
  * @returns {Document} - Respective Document object generated from the retrieved XML.
  */
 export function getXmlDocFromDataUrl(dataUrl) {
     const mimeType = getMimeTypeFromDataUrl(dataUrl);
-    const xmlText = getTextFromDataUrl(dataUrl);
+    const xmlText = getDecodedTextFromDataUrl(dataUrl);
     const xmlParser = new DOMParser();
 
     return xmlParser.parseFromString(xmlText, mimeType || 'text/xml');

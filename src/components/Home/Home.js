00cc66f122dea79128b6b9f0636eccae4240c809
D-Pow <D-Pow@users.noreply.github.com>
2021-06-28 21:58:21 -0600

    Minor: Show an example of font style/size changing in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
 
     return (
         <React.Fragment>
-            <div>Home</div>
+            <div className={'font-brush-script font-size-2em'}>Home</div>
             <button onClick={() => setRedirect('/about')}>Go to About</button>
             <button onClick={() => setRedirect('/animeSearch')}>Go to anime search</button>
         </React.Fragment>

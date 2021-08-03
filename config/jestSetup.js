55d4116539197275aec28cba398a915f3e573fd3
D-Pow <D-Pow@users.noreply.github.com>
2021-08-03 12:41:36 -0600

    Add util method to override window/global properties in jest tests

--- config/jestSetup.js
+++ config/jestSetup.js
@@ -154,3 +154,89 @@ global.Request = jest.fn((url, options) => ({
     url,
     text: () => Promise.resolve(options ? options.body : '')
 }));
+
+
+/*** Utils for direct usage in jest tests ***/
+
+
+/**
+ * Mocks a property of an object with the desired [PropertyDescriptor]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty#description}
+ * configuration.
+ *
+ * The returned `restore()` function should be called once the mocked property is no
+ * longer needed so that the property's original value/configuration can be reset.
+ *
+ * @example
+ * it('does something', () => {
+ *     const restoreMockLocation = mockObjProperty(window, 'location', {
+ *         configurable: true,
+ *         enumerable: true,
+ *         writable: true,
+ *         value: {
+ *             href: 'https://example.com/users/1234',
+ *             origin: 'https://example.com'
+ *         }
+ *     });
+ *     // tests
+ *     restoreMockLocation();
+ * });
+ *
+ * @param {Object} [obj=window] - Object for which to mock a property.
+ * @param {string} property - Property of the `obj` to mock.
+ * @param {PropertyDescriptor} mockDescriptor - Mocked value & configuration.
+ * @returns {function} - Function to restore the mocked property back to its original value.
+ */
+function mockObjProperty(
+    obj = window,
+    property = '',
+    mockDescriptor
+) {
+    const originalDescriptor = {
+        // Default the value in case it was never defined in the first place
+        value: undefined,
+        // It's acceptable to spread `undefined`
+        ...Object.getOwnPropertyDescriptor(obj, property)
+    };
+    // Get a default PropertyDescriptor with `configurable` and `enumerable` set to true.
+    // Add in `writable`/`value` if `value` is specified, otherwise leave it out
+    // because `get`/`set` is being used instead.
+    const getDefaultMockDescriptor = (value) => ({
+        configurable: true, // Property type may be changed; Allows `delete property`.
+        enumerable: true, // Property will be displayed in `Object.keys(obj)` and similar.
+        // Only one of [ writable, value ] or [ get, set ] can be specified.
+        ...(value !== undefined ? {
+            writable: true, // Allows `obj.property = newVal`.
+            value, // Property value. Defaults to `undefined`
+        } : {
+            // Don't fill them in b/c e.g. `get() { return obj[property] }` will recurse indefinitely.
+            // get() {}, // Property getter.
+            // set(newVal) {}, // Property setter.
+        })
+    });
+    const allowedGetterSetterKeys = new Set([ 'get', 'set' ]);
+    const allowedKeys = new Set(
+        Object.keys(getDefaultMockDescriptor(null))
+            .concat(...allowedGetterSetterKeys)
+    );
+
+    if (
+        (mockDescriptor == null)
+        || (typeof mockDescriptor !== typeof {})
+        || !Object.keys(mockDescriptor).every(mockDescriptorKey => allowedKeys.has(mockDescriptorKey))
+    ) {
+        // User didn't pass a PropertyDescriptor.
+        // Automatically fill in the passed value into the default descriptor.
+        // Ensure it's writable by casting an undefined value to null.
+        mockDescriptor = getDefaultMockDescriptor(mockDescriptor !== undefined ? mockDescriptor : null);
+    } else {
+        mockDescriptor = {
+            ...getDefaultMockDescriptor(mockDescriptor.value),
+            ...mockDescriptor,
+        };
+    }
+
+    Object.defineProperty(obj, property, mockDescriptor);
+
+    return () => Object.defineProperty(obj, property, originalDescriptor);
+}
+global.mockObjProperty = mockObjProperty;

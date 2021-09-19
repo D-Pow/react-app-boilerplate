8c569b1c6cc73484bdbe9da5b9d1f869ecaf2b3d
D-Pow <D-Pow@users.noreply.github.com>
2021-09-18 19:07:49 -0600

    Add more description for decorators; use logger to show sample arg values

--- src/utils/Decorators.js
+++ src/utils/Decorators.js
@@ -1,34 +1,68 @@
 /**
+ * Decorates a class or a class' property.
+ * Property can be a variable or function (including getters/setters).
+ *
+ * Decorator will be called *before* the class is actually initialized, so instance variables and
+ * bound methods likely won't appear on `target`.
+ * Similarly, for instance variables/bound methods, the `propertyDescriptor` will often come with a
+ * custom `initializer` function which is used to create the value for the given property. More details [here]{@link https://github.com/tc39/proposal-decorators#2-calling-decorators}.
+ *
  * @typedef {Object} Decorator
- * @property {function} classOrFunction - Class function or class type (i.e. constructor) to pass.
- * @property {string} propertyName - Class instance's function name or null if on a class itself.
- * @property {PropertyDescriptor} propertyDescriptor - Property descriptor of the annotated function/class.
+ * @property {(function|Object)} target - Class (constructor) or class instance that is/owns the property being decorated.
+ *                              Note: Class instances won't have instance variables/arrow functions due to `constructor()` not being called, yet.
+ * @property {(string|undefined)} propertyName - Name of the decorated property; undefined if on the class itself.
+ * @property {(PropertyDescriptor|undefined)} propertyDescriptor - Property descriptor of the decorated property; undefined if on the class itself.
  */
+
+function logArgs(argsAsObj) {
+    const getArrayToPrint = obj => Object
+        .entries(obj)
+        .filter(([ argName, argVal ]) => argVal)
+        .flatMap(([ argName, argVal ]) => ([ `${argName}:`, argVal, '\n' ]));
+    let toPrint = getArrayToPrint(argsAsObj);
+
+    if (Object.keys(argsAsObj).length === 1 && argsAsObj.target) {
+        toPrint = getArrayToPrint(argsAsObj.target);
+    }
+
+    console.log(
+        ...toPrint,
+    );
+}
 
 /**
  * Generic type-definition of a non-arg decorator.
  *
- * @param {Decorator.classOrFunction} targetClassOrFunction
+ * @param {Decorator.target} target
  * @param {Decorator.propertyName} propertyName
  * @param {Decorator.propertyDescriptor} propertyDescriptor
  */
-export function decoratorWithoutArgs(targetClassOrFunction, propertyName, propertyDescriptor) {
-    // Add decorator logic
+export function decoratorWithoutArgs(target, propertyName, propertyDescriptor) {
+    logArgs({
+        target,
+        propertyName,
+        propertyDescriptor,
+    });
 }
 
 /**
  * Generic type-definition of an arg-dependent decorator.
  *
- * @param {...*} - Arguments for an arbitrary decorator requiring params.
+ * @param {...*} decoratorArgs - Arguments for an arbitrary decorator requiring params.
  */
 export function decoratorWithArgs(...decoratorArgs) {
     /**
-     * @param {Decorator.classOrFunction} targetClassOrFunction
+     * @param {Decorator.target} target
      * @param {Decorator.propertyName} propertyName
      * @param {Decorator.propertyDescriptor} propertyDescriptor
      */
-    function actualDecorator(targetClassOrFunction, propertyName, propertyDescriptor) {
-        // Add decorator logic
+    function actualDecorator(target, propertyName, propertyDescriptor) {
+        logArgs({
+            decoratorArgs,
+            target,
+            propertyName,
+            propertyDescriptor,
+        });
     }
 
     return actualDecorator;

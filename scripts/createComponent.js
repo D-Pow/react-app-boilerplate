fe1fb0544435b1e588cb8b38dc5782f47bc65d1f
D-Pow <D-Pow@users.noreply.github.com>
2021-09-28 22:57:15 -0600

    Add new 'typescript' option to createComponent.js

--- scripts/createComponent.js
+++ scripts/createComponent.js
@@ -1,66 +1,110 @@
 const fs = require('fs');
 const parseCliArgs = require('../config/parseCliArgs');
 
-function getClassComponentText(componentName) {
-    return (
-`class ${componentName} extends React.Component {
-    constructor(props) {
-        super(props);
+function getPropTypes(componentName, typescript) {
+    if (typescript) {
+        return '';
     }
+
+    return `
+\n${componentName}.propTypes = {
+
+};\n`;
+}
+
+function getClassComponentText(componentName, typescript) {
+    return (
+`class ${componentName} extends React.Component${typescript ? `<${componentName}Props, any>` : ''} {
+    ${
+        typescript
+            ? ''
+            : `static propTypes = {\n\n    };\n\n    `
+    }static defaultProps = {
+
+    };
 
     render() {
         return (
-            <div></div>
+            <>
+                {this.props.children}
+            </>
         );
     }
 }`
     );
 }
 
-function getFunctionalComponentText(componentName) {
+function getFunctionalComponentText(componentName, typescript) {
+    let functionDefinitionStr =
+`function ${componentName}(props${typescript ? `: ${componentName}Props` : ''} = {})${typescript ? ': React.ReactElement' : ''} {
     return (
-`function ${componentName}(props) {
-    return (
-        <div></div>
+        <>
+            {props.children}
+        </>
     );
-}`
-    );
+}`;
+
+    if (!typescript) {
+        functionDefinitionStr += `\n\n${componentName}.propTypes = {
+
+};\n\n${componentName}.defaultProps = {
+
+};`;
+    }
+
+    return functionDefinitionStr;
 }
 
-function getComponentText(componentName, functional = false) {
+function getComponentText(componentName, { functionalComponent = false, typescript = false } = {}) {
+    let propTypesStrHeader = `import PropTypes from 'prop-types';`;
+
+    if (typescript) {
+        propTypesStrHeader =
+`\ninterface ${componentName}Props {
+    className?: string;
+    children?: React.ReactChildren;
+}`;
+    }
+
     return (
 `import React from 'react';
-import PropTypes from 'prop-types';
+${propTypesStrHeader}
 
-${functional ? getFunctionalComponentText(componentName) : getClassComponentText(componentName)}
-
-${componentName}.propTypes = {
-
-};
-
-${componentName}.defaultProps = {
-
-};
+${functionalComponent ? getFunctionalComponentText(componentName, typescript) : getClassComponentText(componentName, typescript)}
 
 export default ${componentName};
 `
     );
 }
 
-function createComponentInDirectory(componentName, dirName = 'components', functionalComponent = false) {
+function createComponentInDirectory(
+    componentName,
+    {
+        dirName = 'components',
+        functionalComponent = false,
+        typescript = false,
+    } = {},
+) {
     const dir = `./src/${dirName}/${componentName}`;
-    const indexText = `import ${componentName} from './${componentName}';\n\nexport default ${componentName};\n`;
-    const componentText = getComponentText(componentName, functionalComponent);
+    const extension = typescript ? 'tsx' : 'jsx';
+    const indexText = `import ${componentName} from './${componentName}.${extension}';\n\nexport default ${componentName};\n`;
+    const componentText = getComponentText(
+        componentName,
+        {
+            functionalComponent,
+            typescript,
+        },
+    );
 
     fs.mkdir(`${dir}`, { recursive: true }, err => {
         if (err) {
             error(err);
         } else {
-            fs.writeFile(`${dir}/index.js`, indexText, err => {
+            fs.writeFile(`${dir}/index.${extension}`, indexText, err => {
                 if (err) {
                     error(err);
                 } else {
-                    fs.writeFile(`${dir}/${componentName}.js`, componentText, err => {
+                    fs.writeFile(`${dir}/${componentName}.${extension}`, componentText, err => {
                         if (err) {
                             error(err);
                         } else {
@@ -76,7 +120,7 @@ function createComponentInDirectory(componentName, dirName = 'components', funct
 
 function printUsage() {
     const usage = `Creates a component inside its own folder in the src/ directory along with an index.js file.
-    Usage: createComponent [-d|--dir = directoryUnderSrc] [-f|--func] ComponentName
+    Usage: createComponent [-d|--dir = directoryUnderSrc] [-f|--func] [-t|--typescript] ComponentName
     `;
 
     console.log(usage);
@@ -91,23 +135,32 @@ function error(err) {
 function main() {
     const args = parseCliArgs({
         combineShortLongFlags: {
-            isFunctionalComponent: [ 'f', 'func' ],
+            functionalComponent: [ 'f', 'func' ],
             dirName: [ 'd', 'dir' ],
+            typescript: [ 't', 'typescript' ],
         },
         numArgs: {
-            isFunctionalComponent: 0,
+            functionalComponent: 0,
             dirName: 1,
+            typescript: 0,
         },
     });
 
-    const { isFunctionalComponent, dirName } = args;
+    const { functionalComponent, dirName, typescript } = args;
     const componentName = args._?.[0];
 
     if (!componentName) {
         printUsage();
     }
 
-    createComponentInDirectory(componentName, dirName, isFunctionalComponent);
+    createComponentInDirectory(
+        componentName,
+        {
+            dirName,
+            functionalComponent,
+            typescript,
+        },
+    );
 }
 
 main();

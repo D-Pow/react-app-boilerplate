16ca014fc082da8fd3569e50ce1909053434b6aa
D-Pow <D-Pow@users.noreply.github.com>
2021-10-16 20:57:03 -0600

    Simplify output by removing hyphenated and aliased option flags (by default)
    They are superfluous when aliases are specified and just make the resulting object noisier, especially when iterating over them with `Object.entries()`. Add new fields in function options object to toggle them off.

--- config/parseCliArgs.js
+++ config/parseCliArgs.js
@@ -16,6 +16,8 @@ const YargsParser = require('yargs-parser');
  * @param {boolean} [options.sliceAfterFirstDoubleHyphen=false] - Begin arg parsing only after `--`; Use only if script will be called by another script, thus requiring `npm run script1 -- -- args-for-nested-script2`.
  * @param {boolean} [options.clearArgvAfterProcessing=true] - If `argv` should be emptied after arg parsing.
  * @param {boolean} [options.addPlaceholderKeysForUnspecifiedOptions=true] - Adds `myOpt: undefined` to the resulting parsed-options object if the flag wasn't passed by the user.
+ * @param {boolean} [options.removeHyphenatedOptionsFromOutput=true] - If hyphenated version of options should be removed from the resulting parsed-options object (e.g. keep only `myOpt` key instead of both `myOpt` and `'my-opt'`).
+ * @param {boolean} [options.removeOptionAliasesFromOutput=true] - Remove option flag aliases from resulting parsed-options object (e.g. keep only `myOpt` instead of both `myOpt` and `m`).
  * @param {Object<string, string[]>} [options.varNameToFlagAliases] - Mapping of one flag alias (typically the desired camelCase variable name used in your logic) to an array of other flag aliases; The key/val orders don't matter, all will be populated the same.
  * @param {Object} [options.numArgs] - Number of arguments the options take; Use if `-m|--my-arg` doesn't take any arguments and should be cast to a boolean instead of gobbling up subsequent non-arg entries.
  * @param {Object} [options.defaultValues] - Default value for each option (only one needs to be specified); If the flag is a boolean, DO NOT default it to `false`.
@@ -28,6 +30,8 @@ function parseCliArgs(
         sliceAfterFirstDoubleHyphen = false,
         clearArgvAfterProcessing = true,
         addPlaceholderKeysForUnspecifiedOptions = true,
+        removeHyphenatedOptionsFromOutput = true,
+        removeOptionAliasesFromOutput = true,
         varNameToFlagAliases,
         numArgs,
         defaultValues,
@@ -52,7 +56,8 @@ function parseCliArgs(
                 'greedy-arrays': false, // Prevent flags from capturing more values than beyond their `numArgs` specifies
                 'populate--': true, // Set any args after `--` to its own key (`script -a val b c -- d e` => `{ a: 'val', _: [ 'b', 'c' ], '--': [ 'd', 'e' ] }` instead of `_: [ 'b', 'c', 'd', 'e' ]`
                 'set-placeholder-key': addPlaceholderKeysForUnspecifiedOptions, // Adds `optionKey: undefined` to the output object if it isn't specified by the user (shows that the option exists without changing return object functionality)
-                // 'strip-aliased': true, // Remove flag aliases (`varNameToFlagAliases` value) if variable name (`varNameToFlagAliases` key) is specified
+                'strip-dashed': removeHyphenatedOptionsFromOutput, // Remove hyphenated long option flags, leaving only the camelCase option flag values in the resulting object
+                'strip-aliased': removeOptionAliasesFromOutput, // Remove flag aliases (`varNameToFlagAliases` value) if variable name (`varNameToFlagAliases` key) is specified
             },
             alias: varNameToFlagAliases,
             default: defaultValues,

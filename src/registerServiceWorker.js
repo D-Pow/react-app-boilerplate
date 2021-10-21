4e6a7d6cfe44edae3e2450795c3c996ab82f4d7c
D-Pow <D-Pow@users.noreply.github.com>
2021-10-20 19:27:59 -0600

    Minor: Rename function default export to match file name

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -17,7 +17,7 @@ const isLocalhost = Boolean(
     ),
 );
 
-export default function register() {
+export default function registerServiceWorker() {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, self.location);

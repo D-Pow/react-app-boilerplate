8c82e930049d9161f027c82c1b658daa15c489df
D-Pow <D-Pow@users.noreply.github.com>
2021-10-24 17:31:24 -0600

    Allow customization of which router used in `Router`
    Set valid router types in a new config object on Router itself so the parent doesn't have to import the routers itself. Add respective `props.routerProps` for router customization. Add `props.children` for additional rendering opportunities. Sort props in order of decreasing usage likelihood.

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as ReactRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Redirect } from 'react-router-dom';
 
 import SpinnerCircle from '@/components/ui/SpinnerCircle';
 
@@ -57,36 +57,50 @@ export const appRoutes = [
  *
  * @param {Object} props
  * @param {Routes} props.routes - Props used to create `<Route>` entries; Allows child components to specify nested routes themselves (e.g. REST URLs).
- * @param {Object} [props.suspenseProps={fallback: Spinner}] - Fallback used in `<React.Suspense>`.
+ * @param {React.ComponentType} [props.ReactRouter=Router.Types.SLASH] - Type of router to use.
+ * @param {Object} [props.routerProps] - Props for the selected `ReactRouter`.
  * @param {Object} [props.wrapperProps] - Props to pass to the `<div>` inside `<React.Suspense>` that wraps the router.
+ * @param {Object} [props.suspenseProps={fallback: Spinner}] - Fallback used in `<React.Suspense>`.
  * @returns {JSX.Element} - React.Suspense > div > Router > Route[].
  */
 function Router({
+    routes,
+    ReactRouter = Router.Types.SLASH,
+    routerProps = {},
+    wrapperProps = {},
     suspenseProps = {
         fallback: (<SpinnerCircle show={true} />),
     },
-    wrapperProps = {},
-    routes,
+    children,
 } = {}) {
     return (
         <React.Suspense {...suspenseProps}>
             <div {...wrapperProps}>
-                <ReactRouter>
+                <ReactRouter {...routerProps}>
                     <>
                         {routes.map(routeProps => (
                             <Route key={routeProps.path} {...routeProps} />
                         ))}
                     </>
+                    {children}
                 </ReactRouter>
             </div>
         </React.Suspense>
     );
 }
 
+Router.Types = {
+    SLASH: BrowserRouter,
+    HASH: HashRouter,
+};
+
 Router.propTypes = {
     routes: PropTypes.arrayOf(PropTypes.object).isRequired,
+    ReactRouter: PropTypes.elementType,
+    routerProps: PropTypes.object,
     suspenseProps: PropTypes.shape({ fallback: PropTypes.node }),
     wrapperProps: PropTypes.object,
+    children: PropTypes.node,
 };
 
 export default Router;

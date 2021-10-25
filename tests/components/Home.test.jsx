135bdcb53c7478fb72b3336a3ddfe064b3a5de91
D-Pow <D-Pow@users.noreply.github.com>
2021-10-25 00:12:54 -0600

    Add new test for ensuring app redirections work

--- tests/components/Home.test.jsx
+++ tests/components/Home.test.jsx
@@ -1,4 +1,7 @@
+import { fireEvent } from '@testing-library/react';
+
 import Home from '@/components/Home';
+import About from '@/components/About';
 
 describe('Home', () => {
     it('should render the word "Home"', () => {
@@ -6,5 +9,36 @@ describe('Home', () => {
         const homeTextComponent = homeComponent.getByText('Home');
 
         expect(homeTextComponent).toBeDefined();
+    });
+
+    it('should redirect upon clicking a redirect button', async () => {
+        const rootWithHomeComponent = global.renderWithWrappingParent(<Home />);
+        const aboutButton = rootWithHomeComponent.getByText(/Go to About/i);
+        const originalUrl = location.href;
+
+        const newUrl = await global.waitForRedirect(() => {
+            /*
+             * `fireEvent` is already wrapped in `act()` internally, so no need to wrap it here.
+             *
+             * Realistically, we could use the simplified shorthand, `fireEvent.click(elem)`, which
+             * would mean we wouldn't have to use the REQUIRED `bubbles: true` in the constructor.
+             *
+             * However, this version was left as-is to be an example for how to write `fireEvent` with
+             * event constructors, which is helpful for custom events or those not covered by `fireEvent`,
+             * e.g. `Broadcast`.
+             */
+            fireEvent(
+                aboutButton,
+                new MouseEvent('click', { bubbles: true }),
+            );
+        });
+
+        const { element, html } = global.getDomFromRender(rootWithHomeComponent, { fromParent: true });
+
+        expect(html.includes('Home')).toBe(false);
+        expect(element.querySelector(`div.${About.defaultProps.className}`)).toBeDefined();
+        expect(location.href).toEqual(newUrl);
+        expect(location.href).not.toEqual(originalUrl);
+        expect(location.pathname).toEqual('/about');
     });
 });

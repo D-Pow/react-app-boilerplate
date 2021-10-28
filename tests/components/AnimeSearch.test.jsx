21b4c43f96d141f3adb2382e741d19dbe3b4366b
D-Pow <D-Pow@users.noreply.github.com>
2021-10-27 23:04:58 -0600

    Add new test for testing that the `Enter` key also works for searching anime

--- tests/components/AnimeSearch.test.jsx
+++ tests/components/AnimeSearch.test.jsx
@@ -7,7 +7,14 @@ import { getElementMaybe, renderWithWrappingParent, waitForElementVisible } from
 import { narutoResponse } from '/mocks/StaticResponses';
 
 describe('AnimeSearch', () => {
-    it('should search for anime', async () => {
+    /**
+     * Tests that the `<AnimeSearch/>` component renders blank at first and then sends a network request to populate
+     * the search-results section.
+     *
+     * @param {function(): Promise<*>} fireEventToTriggerNetworkCall - Function to call to trigger the network call (e.g. `fireEvent.click()` or `fireEvent.keyDown()`).
+     * @returns {Promise<void>} - Async function containing all the actual tests to be run when verifying network calls are successful.
+     */
+    async function testAnimeSearchWorksForNaruto(fireEventToTriggerNetworkCall) {
         const animeSearchComponent = renderWithWrappingParent(<AnimeSearch />);
         const inputElement = document.querySelector('input[type="search"]');
 
@@ -16,11 +23,24 @@ describe('AnimeSearch', () => {
         expect(await getSearchResultsTitle()).not.toBeDefined();
 
         fireEvent.change(inputElement, { target: { value: 'naruto' }});
-        fireEvent.click(animeSearchComponent.getByText('Search'));
+        await fireEventToTriggerNetworkCall(animeSearchComponent);
 
         await waitForElementVisible(animeSearchComponent, '.search-result');
 
         expect(await getSearchResultsTitle()).toBeDefined();
         expect(animeSearchComponent.container.querySelectorAll('.search-result').length).toEqual(narutoResponse.data.length);
+    }
+
+    it('should search for anime by search-button click', async () => {
+        await testAnimeSearchWorksForNaruto(rootComponent => fireEvent.click(rootComponent.getByText('Search')));
+    });
+
+    it('should search for anime by "Enter" key press', async () => {
+        await testAnimeSearchWorksForNaruto(rootComponent => fireEvent.keyDown(
+            rootComponent.getByText('Search'),
+            {
+                key: 'Enter',
+            },
+        ));
     });
 });

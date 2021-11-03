8ff0b12dcf0f392ed12e01ffeb6af3485dff0209
D-Pow <D-Pow@users.noreply.github.com>
2021-11-03 17:06:15 -0600

    Add example of how to test the `innerText` of an element

--- tests/components/AnimeSearch.test.jsx
+++ tests/components/AnimeSearch.test.jsx
@@ -1,8 +1,13 @@
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, within } from '@testing-library/react';
 
 import AnimeSearch from '@/components/AnimeSearch';
 
-import { getElementMaybe, renderWithWrappingParent, waitForElementVisible } from '/tests';
+import {
+    getDomFromRender,
+    getElementMaybe,
+    renderWithWrappingParent,
+    waitForElementVisible,
+} from '/tests';
 
 import { narutoResponse } from '/mocks/StaticResponses';
 
@@ -12,7 +17,7 @@ describe('<AnimeSearch/>', () => {
      * the search-results section.
      *
      * @param {function(): Promise<*>} fireEventToTriggerNetworkCall - Function to call to trigger the network call (e.g. `fireEvent.click()` or `fireEvent.keyDown()`).
-     * @returns {Promise<void>} - Async function containing all the actual tests to be run when verifying network calls are successful.
+     * @returns {Promise<import('../index.js').RenderedComponent>} - Async function containing all the actual tests to be run when verifying network calls are successful.
      */
     async function testAnimeSearchWorksForNaruto(fireEventToTriggerNetworkCall) {
         const animeSearchComponent = renderWithWrappingParent(<AnimeSearch />);
@@ -29,10 +34,22 @@ describe('<AnimeSearch/>', () => {
 
         expect(await getSearchResultsTitle()).toBeDefined();
         expect(animeSearchComponent.container.querySelectorAll('.search-result').length).toEqual(narutoResponse.data.length);
+
+        return animeSearchComponent;
     }
 
     it('should search for anime by search-button click', async () => {
-        await testAnimeSearchWorksForNaruto(rootComponent => fireEvent.click(rootComponent.getByText('Search')));
+        const animeSearchComponent = await testAnimeSearchWorksForNaruto(
+            rootComponent => fireEvent.click(rootComponent.getByText('Search'))
+        );
+
+        // Two ways to get the `innerText` of a rendered element.
+        //
+        // 1. Use React-testing-library's `within()` to search for content by text within a particular component.
+        //    Note: This also helps to select one element from many with the same text.
+        expect(within(animeSearchComponent.getByRole('button')).getByText('Search')).toBeDefined();
+        // 2. Get the underlying DOM element and get `textContent` since `innerText` isn't defined.
+        expect(getDomFromRender(animeSearchComponent.getByRole('button')).element).toHaveProperty('textContent', 'Search'); // no innerText available
     });
 
     it('should search for anime by "Enter" key press', async () => {

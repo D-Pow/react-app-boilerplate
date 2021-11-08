934134d1e8dde78966c07d0ca7417102104bdb7b
D-Pow <D-Pow@users.noreply.github.com>
2021-11-08 02:40:19 -0700

    Minor: Improve JSDoc for util function

--- src/utils/Network.js
+++ src/utils/Network.js
@@ -1,9 +1,9 @@
 /**
  * Fetches a resource and returns the Base64-encoded result.
  *
- * @param {string} url
- * @param {RequestInit} fetchOptions
- * @returns {Promise<unknown>}
+ * @param {string} url - URL to which the network request will be made.
+ * @param {RequestInit} fetchOptions - Options to pass to the underlying `fetch()` function.
+ * @returns {Promise<unknown>} - The result from the [`FileReader`]{@link https://developer.mozilla.org/en-US/docs/Web/API/FileReader}.
  */
 export async function fetchAsBase64(url, fetchOptions = {}) {
     const res = await fetch(url, fetchOptions);

562ede6d5cce6fdd4c7c292ccd7b839b1770e957
D-Pow <D-Pow@users.noreply.github.com>
2022-02-02 18:04:38 -0700

    Overhaul and enhance `parseCliArgs()` util function
    * Simplify `parseCliArgs()` option-config parameter.
        - Merge different option-config fields (e.g. `numArgs`, `varNameToFlagAliases`, etc.) into one (`optionsConfigs`).
        - Separate option-config formatting logic to their own functions.
    * Improve `parseCliArgs()` config params.
        - Remove superfluous/unnecessary config parameters.
        - Add typedefs for option-config object parameter.
        - Enhance array-type inference by attempting casting to `type[]`.
        - Extract boolean-negation-prefix config string to parameter for customization.
    * Harden `--` stripping logic when parsing CLI args.
        - Don't just check the first CLI arg, but remove all leading `--` args.
    * Add full descriptions for all `yargs-parser` configurations.
        - While most configs are left with the default values, specify them anyway so it's easier to change them later, to understand what they do, and ensure they stay as-is in the future if the default values ever changes from the devs.
    * Move print-usage-help-string logic to `parseCliArgs()` itself.
        - Explain how to make boolean flags `false` if they default to `true` (through `--no-` prefix).
        - Convert camelCase variables (also available as flags) to hyphen-case.
        - Format flags' help-message text by formatting as an evenly-spaced table via `console.table`.
        - Optionally (and by default) exit the process if `--help` is passed.
    * Minor formatting for ease of reading.

--- config/utils/parseCliArgs.js
+++ config/utils/parseCliArgs.js
@@ -1,75 +1,273 @@
 const YargsParser = require('yargs-parser');
 
+
+/**
+ * Configuration for a single option flag coalescing all necessary fields for use
+ * with [yargs-parser]{@link https://www.npmjs.com/package/yargs-parser}.
+ *
+ * @typedef {Object} CliOptionConfig
+ * @property {string} [description] - Flag description to print if `--help` flag is passed.
+ * @property {string} [type] - `typeof` the option's arg(s).
+ * @property {number} [numArgs] - Number of arguments/values that can be passed to the option flag via CLI; Specify `0` to make the flag function as a boolean value.
+ * @property {any} [defaultValue] - Default value for the option's arg(s); If the flag is a boolean, DO NOT default it to `false`.
+ * @property {string[]} [aliases] - Flag aliases that can be used instead of the variable itself (e.g. single-/multi-letter or simplified flag versions).
+ */
+/**
+ * Mapping of the primary, camelCase option flags (each of which will become the variables populated by
+ * the parsed-args function) to their configurations for use with [yargs-parser]{@link https://www.npmjs.com/package/yargs-parser}.
+ *
+ * @typedef {Object<any, CliOptionConfig>} CliOptionsConfigs
+ */
+
+
+/**
+ * Converts a camelCase string to hyphen-case.
+ *
+ * @param {string} str - String to convert.
+ * @returns {string} - The string as hyphen-case.
+ */
+function camelCaseToHyphenCase(str) {
+    return str.replace(/(?<=[a-z])([A-Z])/g, (fullStrMatch, matchGroup) => {
+        // One match group: The capital letter after a lowercase letter.
+        // Lowercase it and prepend a hyphen.
+        return `-${matchGroup.toLowerCase()}`;
+    });
+}
+
+
+/**
+ * Normalizes the flag/options config object by:
+ *
+ * - Populating each unspecified config field with default values.
+ * - Removing prohibited config values (e.g. a `defaultValue` of `null`, `undefined`, or `false`).
+ *
+ * @param {CliOptionsConfigs} cliOptionsConfigs - Config used by `yargs-parser`.
+ */
+function normalizeCliOptionsConfig(cliOptionsConfigs) {
+    Object.entries(cliOptionsConfigs).forEach(([ primaryFlag, flagConfig ]) => {
+        const {
+            type = (
+                (flagConfig.numArgs > 1 && (
+                    (flagConfig.defaultValue?.length > 0 && `${typeof flagConfig.defaultValue[0]}[]`)
+                    ||
+                    'array'
+                ))
+                ||
+                (flagConfig.hasOwnProperty('defaultValue') && typeof flagConfig.defaultValue)
+                ||
+                'boolean'
+            ),
+            numArgs = 0,
+            defaultValue,
+            aliases = [],
+            description = '',
+        } = flagConfig;
+
+        cliOptionsConfigs[primaryFlag] = {
+            type,
+            numArgs,
+            defaultValue,
+            aliases,
+            description,
+        };
+
+        if (flagConfig.defaultValue == null || flagConfig.defaultValue === false) {
+            // Only fill in the default value if it exists.
+            // `false`, `undefined`, and `null` shouldn't be populated.
+            delete cliOptionsConfigs[primaryFlag].defaultValue;
+        }
+    });
+}
+
+
+/**
+ * Inverts the flag-config mappings such that each of the flag's config's inner fields
+ * become top-level fields, each of which holding the values from each individual config.
+ *
+ * Necessary because `yargs-parser` reads the config objects separately rather than altogether.
+ *
+ * @param {CliOptionsConfigs} optionsConfigs - Flag-config mappings.
+ * @returns {import('yargs-parser').yargsParser.Options} - Inverted CLI options config object.
+ */
+function invertCliOptionsConfigs(optionsConfigs) {
+    return Object.entries(optionsConfigs)
+        .reduce((invertedOptionsConfigs, [ primaryFlag, config ]) => {
+            invertedOptionsConfigs.types[primaryFlag] = config.type;
+            invertedOptionsConfigs.aliases[primaryFlag] = config.aliases;
+            invertedOptionsConfigs.numArgs[primaryFlag] = config.numArgs;
+            invertedOptionsConfigs.helpOptionDescriptions[primaryFlag] = config.description;
+
+            if (config.hasOwnProperty('defaultValue')) {
+                invertedOptionsConfigs.defaultValues[primaryFlag] = config.defaultValue;
+            }
+
+            return invertedOptionsConfigs;
+        }, {
+            types: {},
+            aliases: {},
+            defaultValues: {},
+            numArgs: {},
+            helpOptionDescriptions: {},
+        });
+}
+
+
 /**
  * Parse arguments using yargs-parser (simpler and quicker to use than yargs).
  *
- * If calling a script from `npm run`, a double-hyphen is required,
- * e.g. `npm run myScript -- --opt1 val1 -b val2 positional args`
- *
  * Both hyphenated (`--my-flag`) and camelCase (`--myFlag`) forms are accepted.
- *
  * Single-letter flags can be combined into a single hyphen (`-a -b -c` === `-abc`).
+ *
+ * If calling a script from `npm run`, you'll need a double-hyphen to pass flags/args
+ * to the underlying script. If using `yarn`, the double-hyphen is unnecessary.
  *
  * @param {Object} [options]
  * @param {string[]} [options.argv=process.argv] - Arg array to parse.
+ * @param {CliOptionsConfigs} [options.optionsConfigs] - CLI flags/options and their args to parse.
  * @param {boolean} [options.removeNodeAndScriptFromArgs=true] - Remove preceding `node myScript` from args; Set to `false` if not using `process.argv`.
- * @param {boolean} [options.sliceAfterFirstDoubleHyphen=false] - Begin arg parsing only after `--`; Use only if script will be called by another script, thus requiring `npm run script1 -- -- args-for-nested-script2`.
  * @param {boolean} [options.clearArgvAfterProcessing=true] - If `argv` should be emptied after arg parsing.
  * @param {boolean} [options.addPlaceholderKeysForUnspecifiedOptions=true] - Adds `myOpt: undefined` to the resulting parsed-options object if the flag wasn't passed by the user.
  * @param {boolean} [options.removeHyphenatedOptionsFromOutput=true] - If hyphenated version of options should be removed from the resulting parsed-options object (e.g. keep only `myOpt` key instead of both `myOpt` and `'my-opt'`).
  * @param {boolean} [options.removeOptionAliasesFromOutput=true] - Remove option flag aliases from resulting parsed-options object (e.g. keep only `myOpt` instead of both `myOpt` and `m`).
- * @param {Object<string, string[]>} [options.varNameToFlagAliases] - Mapping of one flag alias (typically the desired camelCase variable name used in your logic) to an array of other flag aliases; The key/val orders don't matter, all will be populated the same.
- * @param {Object} [options.numArgs] - Number of arguments the options take; Use if `-m|--my-arg` doesn't take any arguments and should be cast to a boolean instead of gobbling up subsequent non-arg entries.
- * @param {Object} [options.defaultValues] - Default value for each option (only one needs to be specified); If the flag is a boolean, DO NOT default it to `false`.
+ * @param {string} [options.booleanFlagNegationPrefix='no-'] - Prefix to use for negating boolean flags (e.g. `--flag` => true,  `--no-flag` => false).
+ * @param {string} [options.helpMessage] - Top-level description of the CLI script to print if `--help` flag is passed (excludes flag/option help descriptions).
+ * @param {(number|null)} [options.helpExitCode=1] - Exit code to use if `--help` flag is passed (`null` == don't exit).
  * @returns {Object} - Parsed options with the format `key: (value|true)` and `_: [$@]`.
  */
-function parseCliArgs(
-    {
-        argv = process.argv,
-        removeNodeAndScriptFromArgs = true,
-        sliceAfterFirstDoubleHyphen = false,
-        clearArgvAfterProcessing = true,
-        addPlaceholderKeysForUnspecifiedOptions = true,
-        removeHyphenatedOptionsFromOutput = true,
-        removeOptionAliasesFromOutput = true,
-        varNameToFlagAliases,
-        numArgs,
-        defaultValues,
-    } = {},
-) {
+function parseCliArgs({
+    argv = process.argv,
+    optionsConfigs = {},
+    removeNodeAndScriptFromArgs = true,
+    clearArgvAfterProcessing = true,
+    addPlaceholderKeysForUnspecifiedOptions = true,
+    removeHyphenatedOptionsFromOutput = true,
+    removeOptionAliasesFromOutput = true,
+    booleanFlagNegationPrefix = 'no-',
+    helpMessage = '',
+    helpExitCode = 1,
+} = {}) {
     // argv = [ 'path/to/node', 'myScript', ...args ]
     let argvStartIndex = removeNodeAndScriptFromArgs ? 2 : 0;
 
-    if (sliceAfterFirstDoubleHyphen) {
-        argvStartIndex = argv.indexOf('--') + 1;
-
-        if (argvStartIndex === 0) {
-            argvStartIndex = argv.length;
-        }
+    // Handle single/multiple layers of script nesting, e.g.
+    // `npm run scriptThatCallsScript2 -- -- args-for-nested-script2`
+    while (argv[argvStartIndex] === '--') {
+        argvStartIndex++;
     }
 
-    const customArgv = argv.slice(argvStartIndex);
-    const customArgs = YargsParser(
-        customArgv,
+    const filteredArgv = argv.slice(argvStartIndex);
+
+    normalizeCliOptionsConfig(optionsConfigs);
+    const invertedConfigForYargsParser = invertCliOptionsConfigs(optionsConfigs);
+
+    const parsedArgs = YargsParser(
+        filteredArgv,
         {
+            // See: https://github.com/yargs/yargs-parser#configuration
             configuration: {
-                'greedy-arrays': false, // Prevent flags from capturing more values than beyond their `numArgs` specifies
-                'populate--': true, // Set any args after `--` to its own key (`script -a val b c -- d e` => `{ a: 'val', _: [ 'b', 'c' ], '--': [ 'd', 'e' ] }` instead of `_: [ 'b', 'c', 'd', 'e' ]`
-                'set-placeholder-key': addPlaceholderKeysForUnspecifiedOptions, // Adds `optionKey: undefined` to the output object if it isn't specified by the user (shows that the option exists without changing return object functionality)
-                'strip-dashed': removeHyphenatedOptionsFromOutput, // Remove hyphenated long option flags, leaving only the camelCase option flag values in the resulting object
-                'strip-aliased': removeOptionAliasesFromOutput, // Remove flag aliases (`varNameToFlagAliases` value) if variable name (`varNameToFlagAliases` key) is specified
+                // Defaults
+                'short-option-groups': true, // Treat single-hyphen flags with multiple letters as separate flags, e.g. `-abc` == `-a -b -c`.
+                'camel-case-expansion': true, // Append camelCase keys to resulting object if hyphen-case was passed, e.g. `--my-flag` => `{ '--my-flag': val, myFlag: val }`.
+                'dot-notation': true, // Parse dots in flags as objects, e.g. `--foo.bar 3` => `{ foo: { bar: 3 }}`.
+                'parse-numbers': true, // Parse flag args containing numbers within quotes to numbers, e.g. `--foo '5'` => `{ foo: 5 }` instead of `{ foo: '5' }`.
+                'parse-positional-numbers': true, // Same as `parse-numbers` but for positional args.
+                'boolean-negation': true, // Allows `--no-my-flag` to be converted to `{ myFlag: false }` instead of `{ noMyFlag: true }`.
+                'negation-prefix': booleanFlagNegationPrefix, // Sets the prefix used by `boolean-negation`.
+                'duplicate-arguments-array': true, // Convert multiple flag usages into an array, e.g. `-a 1 -a 2` => `{ a: [ 1, 2 ]}`.
+                'flatten-duplicate-arrays': true, // Like `duplicate-arguments-array`, except flattens multiple entries for the flag, e.g. `-a 1 2 -a 3 4` => `{ a: [ 1, 2, 3, 4 ]}`.
+                'halt-at-non-option': false, // Don't stop parsing args after encountering the first unknown flag.
+                'nargs-eats-options': false, // Don't consume hyphen-prefixed args if placed after a flag with an unmet max `numArgs` value, e.g. if `nargs = { a: 2 }`, then`-a b -c` => `{ a: 'b', c: true }` instead of `{ a: [ 'b', '-c' ]}`.
+                'unknown-options-as-args': false, // Parse unknown flags into the flag object rather than putting them in the positional args array.
+
+                // Customizations
+                'greedy-arrays': false, // Prevent flags from capturing more values than beyond their `numArgs` specifies, e.g. `--arr 1 2` => `{ arr: [ 1 ], _: [ 2 ]}`.
+                'populate--': true, // Set any args after `--` to its own key, e.g. `script -a val b c -- d e` => `{ a: 'val', _: [ 'b', 'c' ], '--': [ 'd', 'e' ] }` instead of `_: [ 'b', 'c', 'd', 'e' ]`.
+                'set-placeholder-key': addPlaceholderKeysForUnspecifiedOptions, // Adds `myFlag: undefined` to the output object if it isn't specified by the user; shows that the option exists without changing return object functionality.
+                'strip-dashed': removeHyphenatedOptionsFromOutput, // Remove hyphenated long option flags, leaving only the camelCase option flag values in the resulting object.
+                'strip-aliased': removeOptionAliasesFromOutput, // Remove flag aliases from the output object, leaving only the primary flag (key in the `alias` object) in the output object, e.g. `-n 3` => `{ num: 3 }` instead of `{ num: 3, n: 3 }`.
             },
-            alias: varNameToFlagAliases,
-            default: defaultValues,
-            narg: numArgs,
+            alias: invertedConfigForYargsParser.aliases,
+            default: invertedConfigForYargsParser.defaultValues,
+            narg: invertedConfigForYargsParser.numArgs,
         },
     );
+
 
     if (clearArgvAfterProcessing) {
         argv.splice(0, argv.length);
     }
 
-    return customArgs;
+
+    if (parsedArgs?.help) {
+        printHelpMessageAndExit({
+            helpMessage,
+            helpExitCode,
+            optionsConfigs,
+            booleanFlagNegationPrefix,
+        });
+    }
+
+
+    return parsedArgs;
 }
+
+
+/**
+ * Prints a help message if `--help` or its aliases are specified on the CLI.
+ * Optionally, exits with the specified exit code.
+ *
+ * @param {Object} [options]
+ * @param {string} [options.helpMessage] - Description to print if `--help` flag is passed.
+ * @param {(number|null)} [options.helpExitCode=1] - Exit code to use if `--help` flag is passed (`null` == don't exit).
+ * @param {CliOptionsConfigs} [options.optionsConfigs] - CLI flags/options and their args to parse.
+ * @param {string} [options.booleanFlagNegationPrefix] - Prefix used to negate boolean flags.
+ * @returns {string} - The help message that is printed (if the `exitCode` is null).
+ */
+function printHelpMessageAndExit({
+    helpMessage,
+    helpExitCode,
+    optionsConfigs,
+    booleanFlagNegationPrefix,
+} = {}) {
+    const flagsHelpMessagesMap = Object.entries(optionsConfigs)
+        .reduce((map, [ flag, config ]) => {
+            const allFlagAliases = [ ...config.aliases, flag ]
+                .map(camelCaseToHyphenCase)
+                .map(alias => alias.length === 1
+                    ? `-${alias}`
+                    : `--${alias}`,
+                )
+                .join(', ');
+
+            map[allFlagAliases] = {
+                Type: config.type,
+                'Num Args': config.numArgs,
+                'Default Value': config.hasOwnProperty('defaultValue') ? config.defaultValue : null,
+                Description: config.description || '',
+            };
+
+            return map;
+        }, {});
+
+    const helpMessageToPrint = [
+        helpMessage,
+        '\n',
+        '\n',
+        'Options:',
+        '\n',
+        `\tNote: Boolean flags can be negated by appending \`--${booleanFlagNegationPrefix}\` as a prefix (e.g. \`--flag\` => true,  \`--${booleanFlagNegationPrefix}flag\` => false).`,
+        '\n',
+    ].join('');
+
+    console.log(helpMessageToPrint);
+    console.table(flagsHelpMessagesMap);
+
+    if (helpExitCode != null) {
+        process.exit(helpExitCode);
+    }
+
+    return helpMessageToPrint;
+}
+
 
 module.exports = parseCliArgs;

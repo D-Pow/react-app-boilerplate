6de21730d6f44bf86a7b7a040c37b9b378749971
D-Pow <D-Pow@users.noreply.github.com>
2022-04-12 18:11:19 -0600

    Add new util to get a random color from custom-theme/native-browser color names

--- src/utils/Scss.js
+++ src/utils/Scss.js
@@ -1,4 +1,6 @@
-import { asNumber } from '@/utils/Numbers';
+import { COLORS } from '@/utils/Constants';
+import { asNumber, randomNumber } from '@/utils/Numbers';
+
 import CommonStyles from '@/styles/Common.scss';
 
 
@@ -71,4 +73,56 @@ export function getGridBreakpoints(parsePxStrToNum = true) {
     }
 
     return mapWithPxInStr;
+}
+
+
+/**
+ * Returns a random color from the `colorList`.
+ *
+ * Default `colorList` is the ~148 color names supported across all browsers.
+ *
+ * @param {Object} [options]
+ * @param {string[]} [options.colorList] - List from which to select a random color.
+ * @param {boolean} [options.appendThemeColors] - Add any colors defined in the global `$theme-colors` SCSS variable to the `colorList`.
+ * @param {string | string[]} [options.forbiddenColors] - Specific colors to exclude from the random color selection.
+ * @returns {string} - A random color in the `colorList` (with/out theme colors) that isn't in the `forbiddenColors` list.
+ *
+ * @see [W3Schools color names, hex, color-mixer, and color-picker]{@link https://www.w3schools.com/colors/colors_names.asp}
+ */
+export function getRandomColor({
+    colorList = Object.keys(COLORS),
+    appendThemeColors = true,
+    forbiddenColors = [],
+} = {}) {
+    if (typeof (forbiddenColors) === 'string') {
+        forbiddenColors = [ forbiddenColors ];
+    }
+
+    const forbiddenColorsSet = new Set(forbiddenColors);
+
+    const themeColorsObj = getThemeColors(); // e.g. { primary: '#ffffff', secondary: 'rgba(120, 200, 100, 0.75)' }
+    const colorsObj = {
+        ...themeColorsObj,
+        ...COLORS,
+    };
+    const colorNames = Object.keys(colorsObj);
+    const validColorNames = appendThemeColors ? colorList.concat(colorNames) : colorList;
+
+    let chosenColor;
+
+    do {
+        chosenColor = validColorNames[Math.floor(randomNumber(validColorNames.length))];
+    } while (
+        forbiddenColorsSet.has(chosenColor) // Color names (from the `forbiddenColors` list)
+        || forbiddenColorsSet.has(colorsObj[chosenColor]) // Color hex/rgb(a) values (from `themeColors`, `COLORS`, etc.)
+    );
+
+    // If chosenColor is a theme color, the returned value needs to be a valid CSS value (hex, rgb[a], etc.) so that it
+    // can be used in any CSS field's value.
+    // e.g. Return '#FFFFFF' instead of 'primary'.
+    if (chosenColor in themeColorsObj) {
+        chosenColor = themeColorsObj[chosenColor];
+    }
+
+    return chosenColor;
 }

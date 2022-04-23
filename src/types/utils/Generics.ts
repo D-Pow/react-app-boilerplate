8f3f46b87dd575ae3d97ff2b0061bd347af3670d
D-Pow <D-Pow@users.noreply.github.com>
2022-04-22 21:58:49 -0600

    Replace `ModifyIntersection` name with `ModifyUnion` since `Union` util type now exists
    `ModifyUnion` wouldn't provide much more utility than `Union` does but `ModifyIntersection` could. However, to avoid confusion when using the util type, rename `ModifyIntersection` to `ModifyUnion` since the merging of the two types is based on removing keys from one type and replacing them with another iff they exist in the other (unlike 'Intersection' implies, which is keys they both have in common whilst removing those they don't).

--- src/types/utils/Generics.ts
+++ src/types/utils/Generics.ts
@@ -551,38 +551,7 @@ export type InferGenericBuiltin<T, Default = T> =
  * interface ModifiedType extends ModifyIntersection<OrigType, { b: { d: string, e: number } }> {}
  * type ResultingModifiedType = { a: string, b: { d: string, e: number }}
  */
-export type ModifyIntersection<T, NT> = Omit<T, keyof NT> & NT;
-
-
-/**
- * Joins the keys/values from the type/interface, `O1`, with all the keys/values from
- * the type/interface, `O2`, resulting in the union of the two objects.
- *
- * Companion to `ModifyIntersection` except that any unspecified keys in
- * `O2` don't result in losing the keys/values from `O1`.
- *
- * @example Change field types.
- * interface OrigType { a: object, b: string };
- * interface ModifiedType extends ModifyUnion<OrigType, { b: boolean }> {}
- * type ResultingModifiedType = { a: object, b: boolean }
- * @example Add new fields.
- * interface OrigType { a: object, b: string };
- * interface ModifiedType extends ModifyUnion<OrigType, { c: boolean }> {}
- * type ResultingModifiedType = { a: object, b: string, c: boolean }
- * @example Modify nested fields (keeps unspecified keys).
- * type OrigType = { a: string, b: { c: number } };
- * interface ModifiedType extends ModifyUnion<OrigType, { b: { d: string, e: number } }> {}
- * type ResultingModifiedType = { a: string, b: { c: number, d: string, e: number }}
- */
-export type ModifyUnion<O1 extends Record<string, any>, O2 extends PartialDeep<O1 | any, any>> = {
-    [K in keyof O1]: O2[K] extends never
-        ? O1[K]
-        : O2[K] extends Record<string, any>
-            ? ModifyUnion<O1[K], O2[K]>
-            : O2[K]
-} & (
-    O1 extends Record<string, any> ? Omit<O2, keyof O1> : O1
-);
+export type ModifyUnion<T, NT> = Omit<T, keyof NT> & NT;
 
 
 /**

041568d99eca2b3625c1c8e86f02b95617e2059c
D-Pow <D-Pow@users.noreply.github.com>
2023-05-17 02:23:45 -0600

    Add new util to import TS files in [MC]JS files
    Requires parsing it via `ts-node`

--- config/utils/ESM/Imports.mjs
+++ config/utils/ESM/Imports.mjs
@@ -1,6 +1,11 @@
 import { createRequire } from 'node:module';
-import path from 'node:path';
+import fs from 'node:fs';
 import { pathToFileURL } from 'node:url';
+
+import tsNode from 'ts-node';
+
+import { Paths } from '../Files.js';
+
 
 /**
  * There are a couple ways to import CJS files:
@@ -22,6 +27,99 @@ function requireFile(fileName, baseDir = '') {
     const require = createRequire(baseDir || import.meta.url);
 
     return require(fileName);
+}
+
+
+/**
+ * Imports a TypeScript file for use within JavaScript.
+ * Specifically meant for non-source files (e.g. configs) since Webpack and its loaders
+ * already allows JS to import TS in source code.
+ *
+ * @param {string} filePath - Path to file for importing (can be absolute or relative).
+ * @param {Object} [options]
+ * @param {boolean} [options.includeSourceMap] - Include inline source-map strings in output JS module.
+ * @param {boolean} [options.transpileToCjs] - Transpile .ts down to .cjs rather than to .mjs (this is typically necessary).
+ * @returns {Promise<Module>} - Imported module after transpiling from TS to JS.
+ */
+export async function importTsFileInJs(filePath, {
+    includeSourceMap = false,
+    transpileToCjs = true,
+} = {}) {
+    filePath = Paths.getFileAbsPath(filePath);
+
+    const tsFileContents = fs.readFileSync(filePath).toString();
+    /**
+     * @see [ts-node `create()` compiler docs]{@link https://typestrong.org/ts-node/api/index.html#create}
+     * @see [ts-node `create()` compiler source code]{@link https://github.com/TypeStrong/ts-node/blob/7af5c48864b60576e471da03c064f325ce37d850/src/index.ts#L569}
+     * @type {import('ts-node').Service}
+     */
+    const tsNodeCompiler = tsNode.create({
+        /* Regardless of sourceMap settings, ts-node will always add inline source maps */
+        // compilerOptions: {
+        //     sourceMap: false,
+        //     inlineSourceMap: false,
+        // },
+        ...(transpileToCjs || ({
+            esm: true,
+            moduleTypes: {
+                '**/*.ts': 'esm',
+            },
+        })),
+    });
+
+    const jsTempFilePath = filePath.replace(/\.ts$/, transpileToCjs ? '.js' : '.mjs');
+
+    // Specifying the original file's path as a second arg + adding `moduleTypes['.ts'] = 'esm`
+    // maintains original use of `import`/`export` rather than converting it to CommonJS `require`/`exports`
+    let jsTranspiledCode = tsNodeCompiler.compile(tsFileContents, transpileToCjs ? '' : filePath);
+
+    if (!includeSourceMap) {
+        jsTranspiledCode = jsTranspiledCode.replace(/\/\/# sourceMapping\S+($|\n)/g, '\n');
+    }
+
+    /**
+     * None of the acclaimed methods below worked, either because of trying to load non-ESM within the
+     * context of this ESM file, URIs for `import()` must be of the `file://` schema.
+     * This means we can't import code in a String variable nor Base64 `data:` URIs, regardless of if
+     * the code was only URL-escaped, Base64-encoded, or both),
+     * Plus, apparently loading `Blob` objects works in the browser but not NodeJS.
+     *
+     * Thus, write the transpiled TS (now JS) code to a temp file, then import it like normal, and
+     * delete it after the module is loaded.
+     *
+     * @example Getting Base64 string for `import()`
+     * // btoa(str) === Buffer.from(jsTranspiledCode).toString('base64') !== Buffer.from(jsTranspiledCode).toString('base64url')
+     * const jsTranspiledCodeBase64WithMimeType = `data:application/javascript;base64,${Buffer.from(parseStringTemplate`${jsTranspiledCode}`).toString('base64url')}`;
+     * await import(jsTranspiledCodeBase64WithMimeType);
+     *
+     * @example Plain JS code.
+     * const jsTranspiledCodeUri = `data:application/javascript;charset=utf-8,${jsTranspiledCode}`;
+     * await import(jsTranspiledCodeUri);
+     *
+     * @example Using NodeJS `Module` object.
+     * return new Module()._compile(jsTranspiledCode, filePath).exports;
+     *
+     * @example Creating Object URL instead of manual string creation from previous examples.
+     * const objUrl = URL.createObjectURL(new Blob([ jsTranspiledCodeBase64WithMimeType ], { type: 'application/javascript' }));
+     * // or
+     * const objUrl = URL.createObjectURL(Buffer.from(jsTranspiledCode).buffer);
+     * await import(objUrl);
+     *
+     * @see [Evaluating code via `import(dataUri)`]{@link https://2ality.com/2019/10/eval-via-import.html#evaluating-simple-code-via-import()}
+     * @see [Creating `new Module()` from source code string]{@link https://miyauchi.dev/posts/module-from-string}
+     * @see [Attempting with `eval()` (fails if src code calls `import` or if calling `require()` from .mjs file]{@link https://stackoverflow.com/questions/47978809/how-to-dynamically-execute-eval-javascript-code-that-contains-an-es6-module-re}
+     * @see [Error: 'exports is not defined' when attempting `import()`]{@link https://stackoverflow.com/questions/43042889/typescript-referenceerror-exports-is-not-defined}
+     * @see [Base64-encoding in NodeJS]{@link https://stackoverflow.com/questions/6182315/how-can-i-do-base64-encoding-in-node-js/6182519#6182519}
+     * @see [NodeJS `File` class/API]{@link https://github.com/nodejs/node/pull/47153}
+     */
+
+    fs.writeFileSync(jsTempFilePath, jsTranspiledCode);
+
+    const jsModule = await import(jsTempFilePath);
+
+    fs.rmSync(jsTempFilePath);
+
+    return jsModule;
 }
 
 
@@ -49,6 +147,10 @@ export function importFile(fileName, {
 
     if (onlyFilePath) {
         return resolvedFilePath;
+    }
+
+    if (resolvedFilePath.match(/\.ts\w*$/i)) {
+        return importTsFileInJs(resolvedFilePath);
     }
 
     if (sync) {

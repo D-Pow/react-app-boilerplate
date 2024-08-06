af7eea5dd5bffdd548016a9fad3414ecba925014
D-Pow <D-Pow@users.noreply.github.com>
2024-08-06 11:17:05 -0400

    Add JWT test

---
+++ tests/utils/Jwt.test.ts
@@ -0,0 +1,10 @@
+import { encodeJwt } from '@/utils/Jwt';
+
+describe('JWT', () => {
+    it('should encode JWTs with a body and secret', async () => {
+        const jwtReceived = await encodeJwt(JSON.stringify({ text: 'blah' }), { secret: 'asdf' });
+        const jwtExpected = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXh0IjoiYmxhaCJ9.0nRqiVV4_-MEG6S7jcQgfU7UEJosJ8gO_cgwmWYV56Q';
+
+        expect(jwtReceived).toBe(jwtExpected);
+    });
+});

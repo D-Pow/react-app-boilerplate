28a34f7aab815f9a242c98c559c24d5c2f15ec16
D-Pow <D-Pow@users.noreply.github.com>
2024-08-17 16:02:25 -0400

    Nest JWT test suite inside a new Crypto suite

--- tests/utils/Crypto.test.ts
+++ tests/utils/Crypto.test.ts
@@ -1,11 +1,13 @@
 import { encodeJwt } from '~/config/utils/Crypto';
 
-describe('JWT', () => {
-    it('should encode JWTs with a body and secret', async () => {
-        // TODO - Jest window.crypto mock
-        const jwtReceived = encodeJwt(JSON.stringify({ text: 'blah' }), 'asdf');
-        const jwtExpected = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXh0IjoiYmxhaCJ9.0nRqiVV4_-MEG6S7jcQgfU7UEJosJ8gO_cgwmWYV56Q';
+describe('Crypto', () => {
+    describe('JWT', () => {
+        it('should encode JWTs with a body and secret', async () => {
+            // TODO - Jest window.crypto mock
+            const jwtReceived = encodeJwt(JSON.stringify({ text: 'blah' }), 'asdf');
+            const jwtExpected = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXh0IjoiYmxhaCJ9.0nRqiVV4_-MEG6S7jcQgfU7UEJosJ8gO_cgwmWYV56Q';
 
-        expect(jwtReceived).toBe(jwtExpected);
+            expect(jwtReceived).toBe(jwtExpected);
+        });
     });
 });

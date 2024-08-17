278e6b2ddc6ecfc1b36f6b44f66429c546ed6ed0
D-Pow <D-Pow@users.noreply.github.com>
2024-08-17 15:47:03 -0400

    Fix broken test; Add todo for Jest `crypto.subtle`

--- tests/utils/Jwt.test.ts
+++ tests/utils/Jwt.test.ts
@@ -1,7 +1,8 @@
-import { encodeJwt } from '@/utils/Jwt';
+import { encodeJwt } from '~/config/utils/Crypto';
 
 describe('JWT', () => {
     it('should encode JWTs with a body and secret', async () => {
+        // TODO - Jest window.crypto mock
         const jwtReceived = encodeJwt(JSON.stringify({ text: 'blah' }), 'asdf');
         const jwtExpected = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXh0IjoiYmxhaCJ9.0nRqiVV4_-MEG6S7jcQgfU7UEJosJ8gO_cgwmWYV56Q';
 

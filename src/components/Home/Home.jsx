6368edd202763e555e85b5a6a53145ed68bf6f25
D-Pow <D-Pow@users.noreply.github.com>
2026-06-26 02:00:11 +0200

    Add sample of CSS-Modules and SVG usage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import { Navigate } from 'react-router';
 
+import Image from '@/components/ui/Image';
 import { useServiceWorkerBroadcastChannel } from '@/utils/Hooks';
+import SvgUrl, {
+    ReactComponent as SvgComponent,
+} from '@/assets/react_logo.svg';
 
+import * as styles from './Home.module.scss';
 
 function Home() {
     const [ redirect, setRedirect ] = useState();
@@ -19,7 +24,11 @@ function Home() {
 
     return (
         <>
-            <div className={'font-brush-script font-size-2em'}>Home</div>
+            <div className={'font-brush-script font-size-2em'}>
+                <SvgComponent className={styles.homeWrappingIcon} width={25} />
+                Home
+                <Image className={styles.homeWrappingIcon} src={SvgUrl} alt={SvgComponent.name} aria={{ width: 25 }} />
+            </div>
             <button onClick={() => setRedirect('/about')}>Go to About</button>
             <button onClick={() => setRedirect('/animeSearch')}>Go to anime search</button>
         </>
